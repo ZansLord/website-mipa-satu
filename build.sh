1280d1a9e54df0f0f397cf6cb9bf6f3239be0407
ZinxLord <90433321+ZansLord@users.noreply.github.com>
2023-07-23 17:21:30 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 rm -rf ./dist
 mkdir dist
 npx tailwindcss -i ./input.css -o ./src/css/tailwind.css
+shopt -s dotglob # Enable dotglob option to include hidden files
 cp -r ./src/* dist/
+shopt -u dotglob # Disable dotglob option to avoid unexpected behavior in other commands
 rm ./dist/index.html
 npx html-minifier --collapse-whitespace --remove-comments --remove-redundant-attributes --case-sensitive ./src/index.html -o ./dist/index.html
 rm ./src/css/tailwind.css
-echo '<!--Project: website-mipa-satu | https://github.com/Khuirul-Huda/website-mipa-satu | Khuirul-Huda (@khuirul_huda) -->' > ./dist/index2.html
+echo '<!--Project: website-ix-j | https://github.com/ZansLord | Abdul Malik (@zans.not.lord) -->' > ./dist/index2.html
 cat ./dist/index.html >> ./dist/index2.html
 rm ./dist/index.html
 mv ./dist/index2.html ./dist/index.html
